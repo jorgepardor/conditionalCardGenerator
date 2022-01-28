79863565530fe185892f0cbcf73a3bdd87384e40
jorgepardor <jorgeluis.mail@gmail.com>
2022-01-28 21:06:57 +0000

    Mis cambios al ejercicio

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import "../style/index.css";
-
+/* eslint-disable */
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
@@ -31,19 +31,32 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+      ${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+      <h1>${variables.name ? variables.name : " "} ${
+    variables.lastname ? variables.lastname : " "
+  }</h1>
+      <h2>${variables.role ? variables.role : " "}</h2>
+      <h3>${variables.city ? variables.city : " "} ${
+    variables.country ? variables.country : " "
+  }</h3>
+          
+      <ul class="${variables.socialMediaPosition}">
+        <li><a href="${
+          variables.twitter ? variables.twitter : " "
+        }"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="${
+          variables.github ? variables.github : " "
+        }"><i class="fa-brands fa-github"></i></a></li>
+        <li><a href="${
+          variables.linkedin ? variables.linkedin : " "
+        }"><i class="fab fa-linkedin"></i></a></li>
+        <li><a href="${
+          variables.instagram ? variables.instagram : " "
+        }"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+
+</div>`;
 }
 
 /**
@@ -54,14 +67,14 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://picsum.photos/300/200",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "public/assets/img/2.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
